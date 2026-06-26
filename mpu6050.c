31a651d9b0d9cca10b6ea1d9f8a591a425d03d7f
Feiersea <seanfeiertag@outlook.com>
2026-06-25 22:03:09 -0500

    Added more register definitions and learned more about reading the datasheet.

--- mpu6050.c
+++ mpu6050.c
@@ -24,11 +24,30 @@ struct mpu6050_data {
 	struct mutex lock;
 };
 
-//Register Definitions
-#define MPU6050_PWR_MGMT_1 0x6B
-#define
+/* MPU60X0 Register Map Offsets */
+#define MPU_REG_SMPLRT_DIV	0x19  /* Sample rate register */
+#define MPU6050_REG_CONFIG 	 0x1A /* General Configuration */
+#define MPU6050_REG_GYRO_CONFIG	 0x1B /* Gyroscope configuration, range of outputs */
+#define MPU6050_REG_ACCEL_CONFIG 0x1C /* Configure self test and range for accelerometer */
+#define MPU6050_REG_FIFO_EN	 0x23 /* Enable for the various measurements to be put into the FIFO register */
+#define MPU6050_REG_INT_ENABLE	 0x38 /* For interrupt generation through various methods */
+	#define MPU6050_REG_INT_STATUS	 0x3A /* This shows the status of whether or not there is an interrupt */
 
-//I2C Driver Registration
+
+/* Data Registers */
+
+
+/* Power and Identification Management */
+#define MPU6050_PWR_MGMT_1 	0x6B /* rst, sleep, cycle */
+#define MPU6050_PWR_SLEEP	0x40 //defines the sleep
+#define MPU_PWR_CLKSEL_MASK	0x07 //defines the clearing
+#define MPU6050_CLKSEL_PLL_XGYRO	0x01 //defines the case for x gyro based reference
+
+#define MPU6050_PWR_MGMT_2 	0x6C /* wakectl, standby */
+#
+
+
+//I2C Driver
 static struct i2c_driver mpu_driver = {
 	.driver = {
 		.name = mpu_6050,
@@ -51,8 +70,7 @@ int main() {
 
 	unsigned char buffer[1];
 	if (read(file, buffer, 1) != 1) {
-		perror("failed, to read from the i2c device");
-		return 1;
+		perror("failed, to read from the i2c device")
 	}
 }
 
